ef8c9153e2e6ad79fbcef48dcc7e69904e5f4d58
Vshal007 <73518343+Vshal007@users.noreply.github.com>
2022-05-14 17:41:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,100 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose= require("mongoose");
+const session= require("express-session");
+const passport = require("passport");
+const passportLocalMongoose= require("passport-local-mongoose")
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const app = express();
+
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.use(session({
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialized: false,
+  }))
+
+
+app.use(passport.initialize());
+app.use(passport.session)  
+
+mongoose.connect("mongodb://localhost:27017/userDB")
+
+
+
+const userSchema= new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User= new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.get('/', (req,res)=>{
+    res.render("home")
+})
+
+app.get('/login', (req,res)=>{
+    res.render("login")
+})
+
+app.get('/register', (req,res)=>{
+    res.render("register")
+})
+
+app.get('/secrets', (req,res)=>{
+
+   if(req.isAuthenticated()){ 
+    res.render("register")
+   }else{
+     res.redirect("/login")  
+   }
+})
+
+app.post("/register",(req,res)=>{
+
+ User.register({username:req.body.username}, req.body.password, function(err, user) {
+  if (err) {
+       console.log(err);
+       res.redirect("/register");
+    } else{
+        passport.authenticate("local")(req, res, function(){
+         res.redirect("/secrets")
+        });
+    }
+
+  
+});
+})
+
+
+app.post("/login",(req,res)=>{
+    
+ 
+   
+});
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
+  
